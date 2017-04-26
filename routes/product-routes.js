09d555ca5ef189a04cbb1c120aec0baf9dc58511
Alejandro <ajimenez1724@gmail.com>
2017-04-25 21:09:26 -0400

    exercise commit, 25/3th

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -23,4 +23,23 @@ productRoutes.get('/products/new', (req, res, next) => {
   res.render('prducts.new-product.ejs');
 });
 
+productRoutes.post('/products/new', (req, res, next) => {
+  const theProduct = new Product({
+    name: req.body.productName,
+    price: req.body.productPrice,
+    imgUrl: req.body.productImageUrl,
+    description: req.body.productDescription
+  });
+
+theProduct.save((err) => {
+  if (err) {
+    next(err);
+    return;
+  }
+
+
+  res.redirect('/products');
+});
+});
+
 module.exports = productRoutes;
